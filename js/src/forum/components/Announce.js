4ab7d4761f15a38bf46b03f0d39a85be3d87b335
Bungalow Bill <41351317+zerosonesfun@users.noreply.github.com>
2019-10-13 12:25:51 -0400

    Create Announce.js

---
+++ js/src/forum/components/Announce.js
@@ -0,0 +1,28 @@
+import Component from 'flarum/Component';
+
+export default class Announce extends Component {
+    init() {
+        this.open = false;
+
+        const js = app.forum.attribute('zerosonesfun-announce.js');
+        if (js) {
+            $('body').append(`<script>${js.trim()}</script>`);
+        }
+    }
+
+    view() {
+        return (
+            <div id="Announce" className={this.open && 'showing'}>
+                <div className="container">
+                    <div className="Announce--Content" style={`height: ${this.open ? app.forum.attribute('zerosonesfun-announce-footer.height') || 50 : 0}px;`}>
+                        {m.trust(app.forum.attribute('zerosonesfun-announce.text') || '')}
+                    </div>
+                    <div className="Announce--Icons">
+                        <i className={`Footer--Show fa fa-info-circle ${this.open && 'hidden'}`} onclick={() => (this.open = true)} />
+                        <i className={`Footer--Hide fa fa-caret-down ${!this.open && 'hidden'}`} onclick={() => (this.open = false)} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
