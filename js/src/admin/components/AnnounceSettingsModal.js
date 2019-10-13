ae23fe7c4c92645bc34b0c0506476f5cf8ccff72
Bungalow Bill <41351317+zerosonesfun@users.noreply.github.com>
2019-10-13 13:58:11 -0400

    Update AnnounceSettingsModal.js

--- js/src/admin/components/AnnounceSettingsModal.js
+++ js/src/admin/components/AnnounceSettingsModal.js
@@ -15,16 +15,6 @@ export default class AnnounceSettingsModal extends SettingsModal {
                 <label>{app.translator.trans('zerosonesfun-announce.admin.settings.text')}</label>
                 <textarea rows="5" required className="FormControl" type="text" bidi={this.setting('zerosonesfun-announce.text')} />
             </div>,
-
-            <div className="Form-group">
-                <label>{app.translator.trans('zerosonesfun-announce.admin.settings.height')} (px)</label>
-                <input className="FormControl" type="number" placeholder="50" bidi={this.setting('zerosonesfun-announce.height')} />
-            </div>,
-
-            <div className="Form-group">
-                <label>{app.translator.trans('zerosonesfun-announce.admin.settings.js')}</label>
-                <textarea rows="5" className="FormControl" type="text" bidi={this.setting('zerosonesfun-announce.js')} />
-            </div>,
         ];
     }
 }
