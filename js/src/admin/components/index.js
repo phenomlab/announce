6b2d3982b372191b6fed0c480d7cd921029137dd
Bungalow Bill <41351317+zerosonesfun@users.noreply.github.com>
2019-10-13 12:56:33 -0400

    Update index.js

--- js/src/admin/components/index.js
+++ js/src/admin/components/index.js
@@ -1,6 +1,6 @@
 import app from 'flarum/app';
-import CustomFooterSettingsModal from './components/AnnounceSettingsModal';
+import AnnounceSettingsModal from './components/AnnounceSettingsModal';
 
 app.initializers.add('zerosonesfun-announce', () => {
-    app.extensionSettings['zerosonesfun-announce'] = () => app.modal.show(new CustomFooterSettingsModal());
+    app.extensionSettings['zerosonesfun-announce'] = () => app.modal.show(new AnnounceSettingsModal());
 });
