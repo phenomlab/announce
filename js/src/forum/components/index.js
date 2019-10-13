8a51b8780da66a464ea3cdeb1fa22b27caee1de6
Bungalow Bill <41351317+zerosonesfun@users.noreply.github.com>
2019-10-13 13:59:10 -0400

    Update index.js

--- js/src/forum/components/index.js
+++ js/src/forum/components/index.js
@@ -6,7 +6,6 @@ import Announce from './components/Announce';
 app.initializers.add('zerosonesfun-announce', () => {
     extend(Application.prototype, 'mount', () => {
         const footer = document.createElement('div');
-        footer.setAttribute("id", "Announce-Container");
 
         m.mount(document.body.appendChild(footer), Announce.component());
     });
