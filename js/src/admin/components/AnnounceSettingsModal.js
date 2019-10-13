e8c24eb3a5e0064b6b1bc4f78b090de3fec04508
Bungalow Bill <41351317+zerosonesfun@users.noreply.github.com>
2019-10-13 12:57:05 -0400

    Add files via upload

---
+++ js/src/admin/components/AnnounceSettingsModal.js
@@ -0,0 +1,30 @@
+import SettingsModal from 'flarum/components/SettingsModal';
+
+export default class AnnounceSettingsModal extends SettingsModal {
+    className() {
+        return 'AnnounceSettingsModal Modal--medium';
+    }
+
+    title() {
+        return app.translator.trans('zerosonesfun-announce.admin.settings.title');
+    }
+
+    form() {
+        return [
+            <div className="Form-group">
+                <label>{app.translator.trans('zerosonesfun-announce.admin.settings.text')}</label>
+                <textarea rows="5" required className="FormControl" type="text" bidi={this.setting('zerosonesfun-announce.text')} />
+            </div>,
+
+            <div className="Form-group">
+                <label>{app.translator.trans('zerosonesfun-announce.admin.settings.height')} (px)</label>
+                <input className="FormControl" type="number" placeholder="50" bidi={this.setting('zerosonesfun-announce.height')} />
+            </div>,
+
+            <div className="Form-group">
+                <label>{app.translator.trans('zerosonesfun-announce.admin.settings.js')}</label>
+                <textarea rows="5" className="FormControl" type="text" bidi={this.setting('zerosonesfun-announce.js')} />
+            </div>,
+        ];
+    }
+}
